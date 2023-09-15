43c49c5bc3da54555787d0f162a71569d6519015
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2023-09-15 16:37:03 -0300

    Agregando componenta acciones

---
+++ src/app/modules/learn/learn.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccionesComponent } from './acciones/acciones.component';
+
+
+
+@NgModule({
+  declarations: [
+    AccionesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LearnModule { }
