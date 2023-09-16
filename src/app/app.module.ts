207e3aa0846385fce49e33597ee0264c6e56acc5
Jonathan <jhumanaandia@gmail.com>
2023-09-16 14:11:33 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { TimeLineComponent } from './shared/components';
+import { ContentComponent } from './modules/tutorial/components/content/content.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { TimeLineComponent } from './shared/components';
     AppComponent,
     FooterComponent,
     NavbarComponent,
+    ContentComponent,
    
   ],
   imports: [
