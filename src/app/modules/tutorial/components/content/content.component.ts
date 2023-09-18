63a2fbcf3e2ca2138b3db5a70a07b95f22bccb1d
Jonathan <jhumanaandia@gmail.com>
2023-09-17 23:10:08 -0300

    Se agrega seccion 3 y corrección de logo

--- src/app/modules/tutorial/components/content/content.component.ts
+++ src/app/modules/tutorial/components/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgbProgressbarConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -7,11 +8,19 @@ import { NgbProgressbarConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent {
-  constructor(config: NgbProgressbarConfig) {
+	tema:string='';
+  	constructor(private config: NgbProgressbarConfig,private router : Router) {
 		// customize default values of progress bars used by this component tree
 		config.striped = true;
 		config.animated = true;
 		config.type = 'success';
 		config.height = '20px';
+	}
+	loadTrivia(tema:string): void {
+		this.tema=tema;
+		let url:string='/aprender-';
+		let fullUrl = url.concat(tema);
+		console.log(fullUrl);
+		this.router.navigate([`${fullUrl}`]);
 	}
 }
