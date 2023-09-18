b55608860ef49c319aeef3c105fdc19daecf1966
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2023-09-18 17:42:22 -0300

    eliminando codigo inecesario rotunigy exports

--- src/app/modules/learn/index.ts
+++ src/app/modules/learn/index.ts
@@ -1,8 +1,4 @@
 export *from "./learn.module"
-// export *from "./acciones/acciones.module"
-// export *from "./acciones/"
-// export *from "./interes-compuesto/interes-compuesto.module"
-// export *from "./interes-compuesto/"
 
 export *from "./components/acciones/acciones.module"
 export *from "./components/interes-compuesto/interes-compuesto.module"
--- src/app/modules/learn/learn.module.ts
+++ src/app/modules/learn/learn.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AccionesRoutingModule } from './components/acciones/acciones-routing.module';
-import { InteresCompuestoRoutingModule } from './components/interes-compuesto/interes-compuesto-routing.module';
-import { InstrumentosRoutingModule } from './components/instrumentos/instrumentos-routing.module';
-import { OpcionesRoutingModule } from './components/opciones/opciones-routing.module';
 import { InteresCompuestoModule } from './components/interes-compuesto';
 import { AccionesModule } from './components/acciones';
 import { InstrumentosModule } from './components/instrumentos';
