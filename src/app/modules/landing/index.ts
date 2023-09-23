4460a43afde77c1f5fe438cdc233254a3608b599
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2023-09-23 18:10:40 -0300

    Panel de precio y nuevo forma de componente

---
+++ src/app/modules/landing/index.ts
@@ -0,0 +1,4 @@
+/*****Manager controlador principal */
+export *from "./landing-manager.module"
+/*****Componente particular  */
+export *from "./components/landing-page/landing-page.module"
---
+++ src/app/modules/landing/landing-manager.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingPageModule } from './components/landing-page/landing-page.module';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,LandingPageModule
+  ]
+})
+export class LandingManagerModule { }
