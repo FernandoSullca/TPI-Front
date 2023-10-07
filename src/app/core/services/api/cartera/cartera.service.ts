21e06f82b455170f8663d584b3d704fa36e43288
JonathanHumanaAndia <jhumanaandia@gmail.com>
2023-10-07 00:42:09 -0300

    Se completa valuación detallada de cartera

--- src/app/core/services/api/cartera/cartera.service.ts
+++ src/app/core/services/api/cartera/cartera.service.ts
@@ -2,17 +2,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Cartera } from 'src/app/core/models/valuacion-total-cartera/cartera';
+import { Cartera } from 'src/app/core/models/cartera/cartera';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ValuacionTotalCarteraService {
+export class CarteraService {
   resp = `${environment.API}/cartera/valuacion/total`;
   constructor(private http: HttpClient) {}
 
-  getPreguntas(): Observable<Cartera> {
-    return this.http.get<Cartera>(this.resp);
+  getCartera(): Observable<Cartera> {
+    return this.http.get(this.resp);
   }
 }
--- src/app/features/dashboard/sections/cartera/cartera.component.ts
+++ src/app/features/dashboard/sections/cartera/cartera.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ValuacionTotalCarteraService } from 'src/app/core/services/api/valuacion-total-cartera/valuacion-total-cartera.service';
+import { CarteraService } from 'src/app/core/services/api/cartera/cartera.service';
+import { Cartera } from 'src/app/core/models/cartera/cartera';
 
 @Component({
   selector: 'app-cartera',
   templateUrl: './cartera.component.html',
   styleUrls: ['./cartera.component.scss']
 })
-export class CarteraComponent {
-  constructor(private carteraService : ValuacionTotalCarteraService, private router: Router) { }
+export class CarteraComponent implements OnInit{
   
-  
+  constructor(private carteraService : CarteraService, private router: Router) { }
+
+  cartera:any;
+  ngOnInit(): void {
+    this.getCartera();
+  }
+  getCartera(){
+    return this.carteraService.getCartera().subscribe((response) => {
+      this.cartera=response
+      console.log(this.cartera);
+    });
+  }
+  mostrarValuacionTotalCartera():number{
+    return this.cartera.totalCartera;
+  }
 }
