aa18abe3f9f4548324fa403a5f99450e088a0929
FernandoSullca <sullcafernando18@gmail.com>
2023-10-08 11:45:12 -0300

    Merg conrefactor de archivos repetido ,empezando serializaciond de preguntas subjetivas, Arreglando Front con preguntas genericas(Lista)

--- src/app/features/perfil-subjetivo/stages/stage-one/stage-one.component.ts
+++ src/app/features/perfil-subjetivo/stages/stage-one/stage-one.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Cuestionario, Respuesta, RespuestaBnt } from 'src/app/core/models/initial-profile/questions-profile.model';
+import { CuestionarioInitial, Pregunta, Respuesta, convertirAPreguntaBotones, PreguntaBotones } from 'src/app/core/models/initial-profile/initial-profile.model';
+import { Cuestionario, RespuestaBnt } from 'src/app/core/models/initial-profile/questions-profile.model';
 import { QuestionsProfileService } from 'src/app/core/services/api/subjective-profile/questions-profile.service';
 @Component({
   selector: 'app-stage-one',
@@ -8,12 +9,12 @@ import { QuestionsProfileService } from 'src/app/core/services/api/subjective-pr
   styleUrls: ['./stage-one.component.scss']
 })
 export class StageOneComponent implements OnInit {
- 
-  cuestionario: Cuestionario = {
+
+  cuestionario: CuestionarioInitial = {
     preguntas: [],
   };
 
-  resCuestionario: Cuestionario = {
+  resCuestionario: CuestionarioInitial = {
     preguntas: [],
   };
 
@@ -25,28 +26,60 @@ export class StageOneComponent implements OnInit {
   opcionSeleccionada: number = 0;
   respuestasSeleccionadasPorInstrumento: Record<string, number> = {};
 
-  AnalisisSubjetivo: Record<string, number> = {  };
+  AnalisisSubjetivo: Record<string, number> = {};
   respuestasDeUsuario: { seccion: string, calculo: number }[] = [];
 
   constructor(private profileService: QuestionsProfileService, private router: Router) { }
 
   ngOnInit(): void {
     this.profileService.getCuestionario().subscribe((data) => {
+      console.log("data");
+      console.log(data);
+      console.log("------------------");
       this.resCuestionario = data;
       this.loadQuestions();
     });
+    // this.getTest();
   }
+
+  public testSubjetivo: CuestionarioInitial = {
+    preguntas: []
+  };
+  public PregSubjetivo: CuestionarioInitial = {
+    preguntas: []
+  };
+
+  public getTest() {
+    return this.profileService.obtenerTestSubjetivo()
+      .then((testSubjetivo) => {
+        // this.testSubjetivo.preguntas = testSubjetivo;
+        console.log("Servicio a questionario inicial");
+        console.log(this.testSubjetivo);
+      })
+      .catch((error) => console.error(error))
+  }
+
 
   loadQuestions() {
 
     this.cuestionario.preguntas[0] = this.resCuestionario.preguntas[0];
-
+    const preguntaFiltrada=this.filtrarPreguntasCompuestasBTN(this.resCuestionario);
+    if(preguntaFiltrada){
+      const questions=this.convertirPreguntaBotones(preguntaFiltrada[0]);
+      console.log("    const questions=this.convertirPreguntaBotones(preguntaFiltrada[0]);");
+      console.log(questions);
+      console.log("------------------");
+    }
+    this.PregSubjetivo.preguntas[0] = this.testSubjetivo.preguntas[0];
+    console.log("this.PregSubjetivo");
+    console.log(this.PregSubjetivo);
+    console.log("------------------");
   }
 
   loadNextQuestion(): void {
 
     if (this.resCuestionario) {
-  
+
       this.guardarrespuestas(this.cuestionario.preguntas[0].seccion.nombre,
         this.cuestionario.preguntas[0].TipoComponente);
 
@@ -75,24 +108,40 @@ export class StageOneComponent implements OnInit {
     return Array.isArray(respuestas);
   }
 
-  isArraybnt(respuestas: RespuestaBnt[]): respuestas is RespuestaBnt[] {
-    return Array.isArray(respuestas);
-  }
+  // isArraybnt(respuestas: RespuestaBnt[]): respuestas is RespuestaBnt[] {
+  //   return Array.isArray(respuestas);
+  // }
 
-  isArraybntist(respuestas: RespuestaBnt[]): respuestas is RespuestaBnt[] {
-    return Array.isArray(respuestas);
-  }
+  // isArraybntist(respuestas: RespuestaBnt[]): respuestas is RespuestaBnt[] {
+  //   return Array.isArray(respuestas);
+  // }
 
-  actualizarOpcionesSeleccionadas(seccion: string, pregunta: string, valor: number, seleccionada: boolean) {
-  
-    this.opcionSeleccionada=valor;
-    if (seleccionada) {
-      this.opcionesSeleccionadas.push({ seccion, pregunta, valor });
-    } else {
+  // actualizarOpcionesSeleccionadas(seccion: string, pregunta: string, valor: number, seleccionada: boolean) {
+
+  //   this.opcionSeleccionada=valor;
+  //   if (seleccionada) {
+  //     this.opcionesSeleccionadas.push({ seccion, pregunta, valor });
+  //   } else {
+  //     // Eliminar la opción no seleccionada del arreglo de opciones seleccionadas
+  //     const index = this.opcionesSeleccionadas.findIndex(opcion => opcion.pregunta === pregunta && opcion.valor === valor);
+  //     if (index !== -1) {
+  //       this.opcionesSeleccionadas.splice(index, 1);
+  //     }
+  //   }
+  // }
+
+  actualizarOpcionesSeleccionadas(seccion: string, pregunta: string, valor: number) {
+
+    // SE actualiza para el cadso de los raidus(unica opcion)
+    this.opcionSeleccionada = valor;
+    const index = this.opcionesSeleccionadas.findIndex(opcion => opcion.pregunta === pregunta && opcion.valor === valor);
+    if (index !== -1) {
       // Eliminar la opción no seleccionada del arreglo de opciones seleccionadas
-      const index = this.opcionesSeleccionadas.findIndex(opcion => opcion.pregunta === pregunta && opcion.valor === valor);
-      if (index !== -1) {
-        this.opcionesSeleccionadas.splice(index, 1);
+      this.opcionesSeleccionadas.splice(index, 1);
+
+    } else {
+      // Si noesta en el grupo de opciones ingresadas se guarda
+      this.opcionesSeleccionadas.push({ seccion, pregunta, valor }); if (index !== -1) {
       }
     }
   }
@@ -113,9 +162,9 @@ export class StageOneComponent implements OnInit {
     // Verificar el tipo de pregunta
     let index = -1
     switch (tipo) {
-      case 'checkbox':
+      case 'CHECKBOX':
 
-      
+
         index = this.respuestasDeUsuario.findIndex(respuesta => respuesta.seccion === seccion);
         const valoresCheckbox = this.opcionesSeleccionadas.map(respuesta => respuesta.valor);
         const sumaCheckbox = valoresCheckbox.reduce((total, valor) => total + valor, 0);
@@ -132,8 +181,8 @@ export class StageOneComponent implements OnInit {
         }
         this.AnalisisSubjetivo[seccion] += sumaCheckbox;
         break;
-      case 'radio':
-  
+      case 'RADIO':
+
         index = this.respuestasDeUsuario.findIndex(respuesta => respuesta.seccion === seccion);//Horizonte o riesgo
         let valorRadio = this.opcionSeleccionada;
         console.log('Suma total:', valorRadio);
@@ -149,7 +198,7 @@ export class StageOneComponent implements OnInit {
         }
         this.AnalisisSubjetivo[seccion] += valorRadio;
         break;
-      case 'boton':
+      case 'BOTON':
         index = this.respuestasDeUsuario.findIndex(respuesta => respuesta.seccion === seccion);
         let suma = 0;
         console.log(this.respuestasSeleccionadasPorInstrumento);
@@ -169,30 +218,31 @@ export class StageOneComponent implements OnInit {
         if (!this.AnalisisSubjetivo[seccion]) {
           this.AnalisisSubjetivo[seccion] = 0;
         }
-        this.AnalisisSubjetivo[seccion] +=suma;
+        this.AnalisisSubjetivo[seccion] += suma;
         break;
       default:
         // Tipo de pregunta no reconocido
-        console.error('Tipo de pregunta no reconocido');
+        console.error('Tipo de pregunta no reconocido-para valorizar respuesta');
         break;
     }
     this.opcionesSeleccionadas = [];
   }
 
   loadResultado(): void {
-    this.entregarResultados() ;
+    this.entregarResultados();
     this.router.navigate(['/perfil-inversor-resultado']);
     this.buttonText = 'Continuar';
   }
-  
+
   loadHome(): void {
     // this.entregarResultados() ;
     this.router.navigate(['/dashboard/perfil-inversor']);
     this.buttonText = 'Continuar';
   }
 
- /**********Post de resultados almacenados**********/ 
- public entregarResultados() {
+
+  /**********Post de resultados almacenados**********/
+  public entregarResultados() {
     if (!this.validateData()) {
 
       return false;
@@ -202,15 +252,58 @@ export class StageOneComponent implements OnInit {
         console.log("Enviado");
       })
       .catch((error) => {
-  
+
         console.error(error)
       })
   }
 
   validateData() {
-  return true;
+    return true;
   }
 
+  filtrarPreguntasCompuestasBTN(cuestionario: CuestionarioInitial): Pregunta[] {
+
+    // Filtrar las preguntas de tipo "Boton" que tienen un instrumento no nulo
+    const preguntasBotonConInstrumento: Pregunta[] = cuestionario.preguntas.filter((pregunta) => {
+      return pregunta.TipoComponente === 'BOTON' && pregunta.respuestas[0]?.instrumento !== null;
+    });
+
+    return preguntasBotonConInstrumento;
+
+  }
+  // Nueva función para convertir pregunta a preguntaBotones
+  convertirPreguntaBotones(pregunta: Pregunta): PreguntaBotones {
+
+    return convertirAPreguntaBotones(pregunta);
+  }
+
+  instrumentoMostrado: boolean = false;
+  //Obtiene 
+  opcionesPorInstrumento(respuestasbnts: Respuesta[],instrumento: string): any[] {
+    // Filtrar y ordenar las opciones por instrumento y orden
+    console.log("Funciones unificar respuestas");
+    
+    console.log(respuestasbnts
+    .filter((respuestasbnts) => respuestasbnts.instrumento === instrumento)
+    .sort((a: { orden: number; }, b: { orden: number; }) => a.orden - b.orden));
+    // this.instrumentoMostrado = true;
+    return respuestasbnts
+      .filter((respuestasbnts) => respuestasbnts.instrumento === instrumento)
+      .sort((a: { orden: number; }, b: { orden: number; }) => a.orden - b.orden);
+  }
+
+  esPrimero(respuestasbnts: Respuesta){
+    return respuestasbnts.orden==1;
+  }
+  // getInstrumentosUnicos(respuestas: Respuesta[]): string[] {
+  //   const instrumentosUnicos = new Set<string>();
+  
+  //   respuestas.forEach((respuesta) => {
+  //     instrumentosUnicos.add(respuesta.instrumento);
+  //   });
+  
+  //   return Array.from(instrumentosUnicos);
+  // }
 }
 
 
