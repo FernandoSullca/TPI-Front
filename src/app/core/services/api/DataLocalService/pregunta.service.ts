b6025c80d2e3632be7e51df320108417f3e3afa6
FernandoSullca <sullcafernando18@gmail.com>
2023-10-09 23:46:43 -0300

    Archivos creados para mockear busquedas dinamica de perfil objetivo

--- src/app/core/services/api/DataLocalService/pregunta.service.ts
+++ src/app/core/services/api/DataLocalService/pregunta.service.ts
@@ -10,10 +10,13 @@ export class PreguntaService {
 
 
   private preguntasUrl = 'assets\\mock\\pregunta-objetivas.json'; // Ruta al archivo JSON
+  private preguntasConservadorUrl = 'assets\\mock\\Perfil Objetivo Conservador.json'; // Ruta al archivo JSON
+  private preguntasModeradoUrl = 'assets\\mock\\Perfil Objetivo Moderado.json'; // Ruta al archivo JSON
+  private preguntasArriesgadoUrl = 'assets\\mock\\Perfil Objetivo Arriesgado.json'; // Ruta al archivo JSON
 
   constructor(private http: HttpClient) {}
 
   getPreguntas(): Observable<Pregunta[]> {
-    return this.http.get<Pregunta[]>(this.preguntasUrl);
+    return this.http.get<Pregunta[]>(this.preguntasModeradoUrl);
   }
 }
