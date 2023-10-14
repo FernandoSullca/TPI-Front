48a8362e1d41e009c9c8a250cca34968c6df702a
FernandoSullca <sullcafernando18@gmail.com>
2023-10-14 20:31:46 -0300

    Arregrandotest question..

--- src/app/core/services/api/subjective-profile/questions-profile.service.spec.ts
+++ src/app/core/services/api/subjective-profile/questions-profile.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuestionsProfileService } from './questions-profile.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('QuestionsProfileService', () => {
   let service: QuestionsProfileService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(QuestionsProfileService);
   });
 
