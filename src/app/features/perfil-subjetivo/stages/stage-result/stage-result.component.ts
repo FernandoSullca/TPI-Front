22692b3cfbad9eae61f68e3e6451a8bad6140dfa
FernandoSullca <sullcafernando18@gmail.com>
2023-10-15 21:40:10 -0300

    test preguntas objetivas

--- src/app/features/perfil-subjetivo/stages/stage-result/stage-result.component.ts
+++ src/app/features/perfil-subjetivo/stages/stage-result/stage-result.component.ts
@@ -9,17 +9,13 @@ import { LocalStorageService } from 'src/app/core/services/LocalStorage/local-st
   styleUrls: ['./stage-result.component.scss']
 })
 export class StageResultComponent implements OnInit {
-  // public resultado="Moderado"
+
   valorRecibido: any;
-  perfil: any;
-  public resultado$: Observable<string> | undefined;
   constructor(
     private router: Router,
-    private cdr: ChangeDetectorRef,
     private localStorageService: LocalStorageService) {
-    this.resultado$ = undefined;
+  
   }
-
 
   ngOnInit(): void {
 
@@ -30,11 +26,8 @@ export class StageResultComponent implements OnInit {
 
   }
 
-
   loadHome(): void {
-    // this.entregarResultados() ;
     this.router.navigate(['/dashboard/precios']);
-    // this.buttonText = 'Continuar';
   }
   loadStages() {
     let url: string = './dashboard/perfil-inversor-questions';
