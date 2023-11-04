5793b2c8d0402d0265dc5e98ccf5d1ed023afd21
Jonathan <jhumanaandia@gmail.com>
2023-11-04 18:54:42 -0300

    fix test portafolio-sugerido

--- src/app/core/services/api/portfolio-sugerido/portfolio-sugerido.service.spec.ts
+++ src/app/core/services/api/portfolio-sugerido/portfolio-sugerido.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; 
 import { PortfolioSugeridoService } from './portfolio-sugerido.service';
 
-xdescribe('PortfolioSugeridoService', () => {
+describe('PortfolioSugeridoService', () => {
   let service: PortfolioSugeridoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+    });
     service = TestBed.inject(PortfolioSugeridoService);
   });
 
