6018e9d2737b24b657964a615b2413c2ef9ba195
Machi <josemiguelmc_1998@hotmail.com>
2023-11-10 05:57:48 -0300

    solucion login y cartera por usuario

--- src/app/core/services/api/cartera/cartera.service.ts
+++ src/app/core/services/api/cartera/cartera.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError } from 'rxjs';
 
 import { environment } from 'environments/environment';
@@ -8,6 +8,7 @@ import { HandleErrorApiService } from '../../manejo-errores/handle-error-api.ser
 import axios from 'axios';
 import { Cartera } from 'src/app/core/models/cartera/cartera';
 import { DolarBolsa } from 'src/app/core/models/dolar-bolsa/dolar-bolsa';
+import { LocalStorageService } from '../../LocalStorage/local-storage.service';
 
 
 @Injectable({
@@ -16,17 +17,22 @@ import { DolarBolsa } from 'src/app/core/models/dolar-bolsa/dolar-bolsa';
 export class CarteraService {
 
   resp = `${environment.API}/cartera/valuacion/total`;
-  constructor(private http: HttpClient,private handleErrorService : HandleErrorApiService ) {}
+  constructor(private http: HttpClient, private handleErrorService: HandleErrorApiService, private localStorage: LocalStorageService) { }
 
-  getCartera(): Observable<Cartera>{
-    return this.http.get<Cartera>(this.resp).pipe(
-      catchError((error)=>{
+  getCartera(): Observable<Cartera> {
+    const token = this.localStorage.getItem("token");
+    const headers = new HttpHeaders ({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get<Cartera>(this.resp, { headers }).pipe(
+      catchError((error) => {
         return this.handleErrorService.errorHandler(error);
       })
     );
   }
   // {hola: "mundo"} typeof JSON ....>>>>> DolarBolsa
-  public async acreditarDinero(cantidad : number, concepto : string) {
+  public async acreditarDinero(cantidad: number, concepto: string) {
     const body = {
       "cantidadPorAcreditar": cantidad,
       "concepto": concepto
@@ -40,13 +46,13 @@ export class CarteraService {
       // Puedes mostrar un mensaje de error o realizar otras acciones de manejo de errores aquí
     }
     // const resp = await axios.post(`${environment.API}/cartera/acreditar/dinero`, body);
-  
-  } 
-  
-  getPrecioDolarMEP(): Observable<DolarBolsa>{
-    const urlBolsa ='https://dolarapi.com/v1/dolares/bolsa';
+
+  }
+
+  getPrecioDolarMEP(): Observable<DolarBolsa> {
+    const urlBolsa = 'https://dolarapi.com/v1/dolares/bolsa';
     return this.http.get<DolarBolsa>(urlBolsa).pipe(
-      catchError((error)=>{
+      catchError((error) => {
         return this.handleErrorService.errorHandler(error);
       })
     )
