67261f4f6a11b2405834a391292f9614df431fb0
FernandoSullca <sullcafernando18@gmail.com>
2023-11-17 13:57:56 -0300

    recuperar perfi durante el login para buscar test test objetivo

--- src/app/presentation/features/login/login.component.ts
+++ src/app/presentation/features/login/login.component.ts
@@ -146,7 +146,7 @@ export class LoginComponent implements OnInit {
       this.LocalStorageService.setPerfilSubjetivo(perfilUsuario);
       this.LocalStorageService.SetPerfilActualLocal();
       ///Verifica si el test que completo esra el objetivo , ebn caso de serlo actualiza la flag que usa cartera, y muestra el perfil obtenido
-      
+      this.LocalStorageService.setItem('perfilinversor', perfilUsuario.perfilInversor);
       if(perfilUsuario.tipoNivelConocimiento!=null||perfilUsuario.nivelConocimiento!=null){
       console.log("🚀 ~ file: login.component.ts:151 ~ LoginComponent ~ AlmacenarUsuario_Perfil ~ perfilUsuario.tipoNivelConocimiento!=null||perfilUsuario.tipoNivelConocimiento!", perfilUsuario.tipoNivelConocimiento!=null||perfilUsuario.nivelConocimiento!=null)
       console.log("🚀 ~ file: login.component.ts:150 ~ LoginComponent ~ AlmacenarUsuario_Perfil ~ tipoNivelConocimiento:", perfilUsuario.tipoNivelConocimiento)
