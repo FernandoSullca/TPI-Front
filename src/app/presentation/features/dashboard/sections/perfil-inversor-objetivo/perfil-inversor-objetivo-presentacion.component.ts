c19c50d67f79e3ca227420b0c737a952aa60ef9d
Jonathan <jhumanaandia@gmail.com>
2023-11-16 23:02:16 -0300

    fix cartera para usuarios con/sin perfil

--- src/app/presentation/features/dashboard/sections/perfil-inversor-objetivo/perfil-inversor-objetivo-presentacion.component.ts
+++ src/app/presentation/features/dashboard/sections/perfil-inversor-objetivo/perfil-inversor-objetivo-presentacion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,13 @@ import { Router } from '@angular/router';
   templateUrl: './perfil-inversor-objetivo-presentacion.component.html',
   styleUrls: ['./perfil-inversor-objetivo-presentacion.component.scss']
 })
-export class PerfilInversorObjetivoPresentacionComponent {
-
+export class PerfilInversorObjetivoPresentacionComponent implements OnInit{
+  perfilObjetivoActual? : string|null|undefined;
   constructor(private router: Router) {
     // customize default values of progress bars used by this component tree
+  }
+  ngOnInit(): void {
+    this.obtenerPerfilObjetivo()
   }
 
   loadStages() { 
@@ -18,5 +21,12 @@ export class PerfilInversorObjetivoPresentacionComponent {
   }
   loadHome(): void {
     this.router.navigate(['/dashboard/precios']);
+  }
+  obtenerPerfilObjetivo(){
+    const resultadoObjetivo = localStorage.getItem('perfilinversor');
+    if(resultadoObjetivo){
+      this.perfilObjetivoActual = JSON.parse(resultadoObjetivo);
+    }
+    
   }
 }
